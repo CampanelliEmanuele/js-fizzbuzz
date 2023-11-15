3a3dbf67389bee1efda7656973d89cd19381ce6a
CampanelliEmanuele <emanuele.campanelli2001@gmail.com>
2023-11-15 16:28:02 +0100

    Add: comments

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 // Si scorrono i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
 
-  if (i % 3 == 0 && i % 5 == 0) {
+  if (i % 3 == 0 && i % 5 == 0) { // Se i e' divisibile sia per 3 che per 5
     console.log("FizzBuzz")
   } else if (i % 3 == 0) {   // Se i e' divisibile per 3
     console.log("Fizz")
   } else if (i % 5 == 0) {   // Se i e' divisibile per 5
     console.log("Buzz")
-  } else {
+  } else {                  // Altrimenti si stampa semplicemente il numero
     console.log(i)
   }
 
